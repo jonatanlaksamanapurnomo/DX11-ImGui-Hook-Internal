c003f74f6ed305a6ad8d631b5013c4eb68c003c5
xo1337 <68725291+xo1337@users.noreply.github.com>
2021-06-15 08:28:48 -0600

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,6 +1,13 @@
 #include <Windows.h>
 #include "vars.hpp"
 
+/*
+Credits: https://github.com/rdbo/ImGui-DirectX-11-Kiero-Hook
+This was made by xo1337, i've used the rdbo ImGui DX11 Hook as a base,
+but in this repo, made everything more clean and beginner friendly.
+*/
+
+
 Vars* vars = new Vars();
 
 extern DWORD D3DThread();
